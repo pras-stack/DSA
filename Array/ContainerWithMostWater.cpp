8f80090e349ef74b28ce1e2c9e0277e62ed0e91d
Prastuti Mazumdar <66267894+pras-stack@users.noreply.github.com>
2022-02-08 13:55:04 +0530

    Create ContainerWithMostWater.cpp

---
+++ Array/ContainerWithMostWater.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/container-with-most-water/
+
+Algorithm : Two Pointer, Greedy
+
+class Solution {
+public:
+    int maxArea(vector<int>& h)
+    {
+        int maxArea =0  , start =0, end = h.size()-1;
+        while(start <end)
+        {
+            int height = min(h[start], h[end] );
+            int width = end- start;
+            
+            int currentArea = height*width;
+            maxArea = max(currentArea, maxArea);
+            
+            if(h[start] < h[end])
+            {
+                start ++;
+            }
+            else
+            {
+                end--;
+            }
+        }
+            
+        
+        return maxArea;
+    }
+};
