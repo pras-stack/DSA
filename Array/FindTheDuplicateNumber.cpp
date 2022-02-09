e6ceb0eb234c4d15c8eca9a8af0944963c344446
Prastuti Mazumdar <66267894+pras-stack@users.noreply.github.com>
2022-02-09 12:54:11 +0530

    Create FindTheDuplicateNumber.cpp

---
+++ Array/FindTheDuplicateNumber.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/find-the-duplicate-number/
+
+// Algorithm: Two pointer,  bit manipulation, Binary Search 
+
+class Solution {
+public:
+    int findDuplicate(vector<int> &nums)
+  {
+    int slow = nums[0], fast = nums[0];
+    do
+    {
+      slow = nums[slow];
+      fast = nums[nums[fast]];
+    } while (slow != fast);
+
+    fast = nums[0];
+    while (slow != fast)
+    {
+      slow = nums[slow];
+      fast = nums[fast];
+    }
+    return fast;
+        
+    }
+};
